7f359c51748b300a64547a934bec63e864555474
Pedro Fernández <pedrofernandez@MacBook-Air-de-Pedro.local>
2022-11-12 15:35:34 -0300

    Mejorando frontend

--- app/router.go
+++ app/router.go
@@ -10,6 +10,7 @@ func MapUrls(router *gin.Engine, dependencies *Dependencies) {
 	// Products Mapping
 
 	router.GET("/items/:id", dependencies.ItemController.Get)
+	//router.GET("/items", dependencies.ItemController.Get)
 	router.POST("/items", dependencies.ItemController.Insert)
 
 	fmt.Println("Finishing mappings configurations")
